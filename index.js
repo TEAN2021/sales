0455c23083310f8edd466cbd488bd0a287167bda
7eoz <thesandrini@gmail.com>
2021-10-11 01:16:05 -0300

    adding migrations

---
+++ index.js
@@ -0,0 +1,16 @@
+// require('dotenv-safe').config();
+// const express = require('express');
+// const app = express();
+// const router = require('./route');
+// require('./db');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(process.env.SYSTEM_PORT, () => {
+	console.log('Server is running at localhost: ', process.env.SYSTEM_PORT);
+});
+
+module.exports = app;
